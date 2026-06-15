79d11b58480a7ad1fd7676af57c18d1e02852d07
Kalpana3007 <naikodikalpana07@gmail.com>
2026-06-15 21:27:10 +0530

    Fix CORS preflight route

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
-app.options('*', cors())
+app.options(/.*/, cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
